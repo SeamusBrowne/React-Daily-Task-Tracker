ca154da839448b217c1514019c821b0ca9f7bbe0
Seamus Browne <31517569+SeamusBrowne@users.noreply.github.com>
2023-11-03 23:22:31 +0000

    Update Task.js

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,1 +1,22 @@
+import React, { useState } from "react";
 
+function Task(props) {
+
+    const { id, title} = props.task;
+
+    return  (
+        <>
+                <label>
+                    <input type="checkbox" onChange={() => setIsChecked(!isChecked)}
+                    />
+                    {title}
+                </label>
+                <button className="task_delete" onClick={() => props.removeTask(props.columnId, id)}>X</button>
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default Task;
