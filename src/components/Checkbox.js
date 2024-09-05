6073b47983e8180d58598d63cdd7b84109424090
Seamus Browne <31517569+SeamusBrowne@users.noreply.github.com>
2024-09-05 23:51:14 +0100

    Update Checkbox.js

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,1 +1,13 @@
+import React from 'react';
 
+function Checkbox({ isChecked, onChange }) {
+  return (
+    <input
+      type="checkbox"
+      checked={isChecked}
+      onChange={onChange}
+    />
+  );
+}
+
+export default Checkbox;
