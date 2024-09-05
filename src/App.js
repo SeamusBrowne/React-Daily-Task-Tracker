defd76ebfa0a771950567e4cd614818e4a42197d
Seamus Browne <31517569+SeamusBrowne@users.noreply.github.com>
2024-09-05 23:50:10 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import Column from './Column';
+import Countdown from './Countdown';
 
+function App() {
+  return (
+    <div className="App">
+      <h1>Daily Task Tracker</h1>
+      <Countdown />
+      <Column />
+    </div>
+  );
+}
+
+export default App;
