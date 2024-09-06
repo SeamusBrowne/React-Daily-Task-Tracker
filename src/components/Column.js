49f2396cf7f0c90b9596f86c10bd0b032201f2fa
Seamus Browne <31517569+SeamusBrowne@users.noreply.github.com>
2024-09-06 01:08:41 +0100

    Update Column.js

--- src/components/Column.js
+++ src/components/Column.js
@@ -1,1 +1,16 @@
+import React, { useState } from 'react';
+import Task from './Task';
+import AddTask from './AddTask';
 
+function Column() {
+  const addTask = (title) => {
+    
+  };
+  
+  return (
+    <div className="column">
+    
+    </div>
+  );
+}
+export default Column;
