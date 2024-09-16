3f8892be3ed100ccb0d4a5a25bd126d4965340d1
Seamus Browne <31517569+SeamusBrowne@users.noreply.github.com>
2024-09-16 23:57:48 +0100

    Update AddTask.js

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -12,26 +12,15 @@ function AddTask({ onAddTask }) {
   };
 
   return (
-    <div className="add_task">
-      {isAddible ? (
-        <form
-            className={`add_task_edit ${props.editClass ? props.editClass : ""}`}
-            onSubmit={submission}
-            >
-            <input type="text" value={inputText} placeholder={props.placeholder || props.text} onChange={(event) => setInputText(event.target.value)} autoFocus/>
-            <div className="add_task_edit_footer">
-              <button type="submit">{props.buttonText || "Add"}</button>
-              <button className="delete_add">X</button>
-            </div>
-        </form>
-      ) : (
-        <p
-        className={`add_task_text ${props.displayClass ? props.displayClass : ""}`}
-        onClick={() => setisAddible(true)}>
-          {props.text}
-        </p>
-      )}
-    </div>
+    <form onSubmit={submission}>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="Add a new task"
+      />
+      <button type="submit">Add Task</button>
+    </form>
   );
 }
 
